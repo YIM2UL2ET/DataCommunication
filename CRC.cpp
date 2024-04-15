b3561f8cc4082b20c174754a6d2a4d6148c791a8
임승호 <132066506+YIM2UL2ET@users.noreply.github.com>
2024-04-15 22:45:07 +0900

    업로드

---
+++ CRC.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+class dataWordManage
+{
+protected:
+    vector <bool> ChangeDataWord(uint8_t data) {
+        vector <bool> dataWord(7);
+        for (int i = 0; i < 7; i++) {
+            dataWord[6-i] = data & (uint8_t(0x1) << i);
+        }
+        return dataWord;
+    }
+
+    vector <vector <bool>> ChangeDataWord(vector <uint8_t> datas) {
+        vector <vector <bool>> dataWord;
+        for (bool data : datas) dataWord.push_back(ChangeDataWord(data));
+        return dataWord;
+    }
+
+    vector <vector <bool>> ChangeDataWord(string datas) {
+        vector <vector <bool>> dataWord;
+        for (auto data : datas) dataWord.push_back(ChangeDataWord(data));
+        return dataWord;
+    }
+};
+
+class CRC: dataWordManage
+{
+private:
+    int polySize;
+    vector <bool> polynomial;
+
+    void Calculate(vector <bool> &dataCode) {
+        while (dataCode.size() > polySize-1) {
+            if (dataCode.front()) {
+                for (int i = 0; i < polySize; i++) {
+                    if (dataCode[i] != polynomial[i]) dataCode[i] = 1;
+                    else dataCode[i] = 0;
+                }
+            }
+            dataCode.erase(dataCode.begin());
+        }
+    }
+
+public:
+    CRC (string poly) {
+        for (char bit : poly) {
+            if (bit == '1') polynomial.push_back(1);
+            else polynomial.push_back(0);
+        }
+        polySize = poly.size();
+    }
+
+    vector <vector <bool>> ReturnCW(string data) {
+        return ReturnCW(ChangeDataWord(data));
+    }
+
+    vector <vector <bool>> ReturnCW(vector <vector <bool>> dataCodes) {
+        for (auto &dataCode : dataCodes) dataCode = ReturnCW(dataCode);
+        return dataCodes;
+    }
+
+    vector <bool> ReturnCW(uint8_t data) {
+        return ReturnCW(ChangeDataWord(data));
+    }
+
+    vector <bool> ReturnCW(vector <bool> dataWord) {
+        vector <bool> codeWord = dataWord;
+        for (int i = 0; i < polySize-1; i++) dataWord.push_back(0);
+        Calculate(dataWord);
+        for (auto bit : dataWord) codeWord.push_back(bit);
+        return codeWord;
+    }
+
+    bool Check(vector <bool> codeWord) {
+        Calculate(codeWord);
+        for (auto bit : codeWord) {
+            if (bit) return false;
+        }
+        return true;
+    }
+
+    bool Check(vector <vector <bool>> codeWords) {
+        for (auto codeWord : codeWords) {
+            if (!Check(codeWord)) return false;
+        }
+        return true;
+    }
+
+    int CWSize(void) {
+        return polySize + 7;
+    }
+};
+
+int main(void) {
+    string data, poly;
+    char ch;
+
+    ifstream in_file("text.txt");
+    if (!in_file.is_open()) exit(1);
+    while (in_file.get(ch)) data.push_back(ch);
+    in_file.close();
+
+    cout << "Input polynomial in bit : ";
+    cin >> poly;
+
+    CRC crc(poly);
+
+    auto codeWords = crc.ReturnCW(data);
+    
+    int cnt = 0, cws = crc.CWSize();
+    cout << "data's codeWords : \n";
+    for (auto cw : codeWords) {
+        for (auto bit : cw) cout << bit;
+        if (cnt > 100) cout << '\n', cnt = 0;
+        else cout << " ", cnt += cws;
+    }
+    cout << '\n';
+
+    if(crc.Check(codeWords)) cout << "Error is nothing";
+    else cout << "this code word is Error";
+
+    return 0;
+}
---
+++ Parity.cpp
@@ -0,0 +1,158 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+class dataWordManage
+{
+protected:
+    vector <bool> ChangeDataWord(uint8_t data) {
+        vector <bool> dataWord(7);
+        for (int i = 0; i < 7; i++) {
+            dataWord[6-i] = data & (uint8_t(0x1) << i);
+        }
+        return dataWord;
+    }
+
+    vector <vector <bool>> ChangeDataWord(vector <uint8_t> datas) {
+        vector <vector <bool>> dataWord;
+        for (bool data : datas) dataWord.push_back(ChangeDataWord(data));
+        return dataWord;
+    }
+
+    vector <vector <bool>> ChangeDataWord(string datas) {
+        vector <vector <bool>> dataWord;
+        for (auto data : datas) dataWord.push_back(ChangeDataWord(data));
+        return dataWord;
+    }
+};
+
+class Parity : dataWordManage
+{
+public:
+    bool ParityBit(uint8_t byte) {
+        return ParityBit(ChangeDataWord(byte));
+    }
+
+    bool ParityBit(vector <bool> dataWord) {
+        bool Parity = true;
+        for (bool data : dataWord) {
+            if (data) Parity = !Parity;
+        }
+        return Parity;
+    }
+
+    vector <bool> FindCW(uint8_t byte) {
+        return FindCW(ChangeDataWord(byte));
+    }
+
+    vector <bool> FindCW(vector <bool> dataWord) {
+        vector <bool> codeWord = dataWord;
+        codeWord.push_back(ParityBit(dataWord));
+        return codeWord;
+    }
+
+    vector <vector <bool>> FindCW(vector <uint8_t> datas) {
+        return FindCW(ChangeDataWord(datas));
+    }
+
+    vector <vector <bool>> FindCW(string datas) {
+        return FindCW(ChangeDataWord(datas));
+    }
+
+    vector <vector <bool>> FindCW(vector <vector <bool>> dataWords) {
+    int size = dataWords.size();
+        vector <vector <bool>> codeWord;
+        vector <bool> colParity(8, true);
+        for (auto dataWord : dataWords) {
+            codeWord.push_back(FindCW(dataWord));
+            for (int i = 0; i < 7; i++) {
+                if (dataWord[i]) colParity[i] = !colParity[i];
+            }
+        }
+        codeWord.push_back(colParity);
+        return codeWord;
+    }
+
+    bool Inspection(vector <vector <bool>> codeWord) {
+        int size = codeWord.size()-1;
+        vector <bool> colParity(7, true);
+        for (int i = 0; i < size; i++) {
+            if (!Inspection(codeWord[i])) return false;
+            for (int j = 0; j < 7; j++) {
+                if (codeWord[i][j]) colParity[j] = !colParity[j];
+            }
+        }
+        
+        for (int i = 0; i < 7; i++) {
+            if (colParity[i] != codeWord.back()[i]) return false;
+        }
+        return true;
+    }
+
+    bool Inspection(vector <bool> codeWord) {
+        bool Parity = true;
+        for (int i = 0; i < codeWord.size()-1; i++) {
+            if (codeWord[i]) Parity = !Parity;
+        }
+        return codeWord.back() == Parity;
+    }
+};
+
+void PrintParityMatrix(vector <vector <bool>> Parity2D) {
+    for (int i = 0; i < Parity2D.size()-1; i++) {
+        for (int j = 0; j < Parity2D[i].size()-1; j++) {
+            cout << ' ' << Parity2D[i][j];
+        }
+        cout << '|' << Parity2D[i].back() << '\n';
+    }
+    cout << "--------------+--\n";
+    for (int j = 0; j < Parity2D.back().size()-1; j++) {
+        cout << ' ' << Parity2D.back()[j];
+    }
+    cout << '|' << Parity2D.back().back() << '\n';
+}
+
+void Parity1D(void) {
+    char ch;
+    Parity p;
+
+    cout << "Input character : ";
+    cin >> ch;
+    auto codeWord = p.FindCW(ch);
+
+    codeWord[0] = !codeWord[0];
+    codeWord[1] = !codeWord[1];
+    
+    cout << ch << "'s parity code word is : ";
+    for (bool bit : codeWord) cout << bit;
+    if (p.Inspection(codeWord)) cout << "\nError is nothing\n\n";
+    else cout << "\nthis code word is Error\n\n";
+}
+
+void Parity2D(void) {
+    string data;
+    char ch;
+    Parity p;
+
+    cout << "Input file name : ";
+    cin >> data;
+
+    ifstream in_file(data);
+    if (!in_file.is_open()) exit(1);
+    while (in_file.get(ch)) data.push_back(ch);
+    in_file.close();
+
+    vector <vector <bool>> Parity2D = p.FindCW(data);
+
+    PrintParityMatrix(Parity2D);
+    if (p.Inspection(Parity2D)) cout << "Error is nothing\n";
+    else cout << "this code word is Error\n";
+}
+
+int main(void) {
+    Parity1D();
+    Parity2D();
+
+    return 0;
+}
